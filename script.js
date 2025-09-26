018b6191a187ec93b907006e4ad03ff0a8766190
1ns1ze <156232524+1ns1ze@users.noreply.github.com>
2025-09-26 16:00:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function togglePassword() {
-  const passwordField = document.getElementById("password");
-  passwordField.type = passwordField.type === "password" ? "text" : "password";
+const form = document.getElementById("registerForm");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+const closeModal = document.getElementById("closeModal");
+const userName = document.getElementById("userName");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  let firstname = document.getElementById("firstname").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  if (firstname === "" || email === "" || password === "") {
+    alert("Please fill in all fields!");
+    return;
+  }
+
+  userName.textContent = firstname;
+  modal.style.display = "block";
+});
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+closeModal.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 }
